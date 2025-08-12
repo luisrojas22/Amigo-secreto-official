ddf94f9d406817ff8766cd6e8b070bdbb51a3365
Luis Angel <noble6byhalo@gmail.com>
2025-08-11 19:20:36 -0500

    Avanze falta funcion sortep

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaGeneral = []
+ function agregarAmigo(){
+let nuevoNombre = document.getElementById("amigo").value
+if (nuevoNombre === "") {
+    alert('Por favor ingrese un un valor válido')
+    return
+} else{
+listaGeneral.push(nuevoNombre)
+document.getElementById("amigo").value = ""
+console.log(listaGeneral)
+mostrarLista();
+}
+ }
+ function mostrarLista(){
+    let contenedor = document.getElementById("listaAmigos");
+    contenedor.innerHTML = ""
+    listaGeneral
+ }
