a76536895ec9c12288829ccb7b7bd687c3923d48
Luis Angel <noble6byhalo@gmail.com>
2025-08-12 15:45:02 -0500

    version Final

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+// Declaracion de variables generales
 let listaGeneral = []
+// Funciones
+// Función principal
  function agregarAmigo(){
 let nuevoNombre = document.getElementById("amigo").value
 if (nuevoNombre === "") {
-    alert('Por favor ingrese un un valor válido')
+    alert('Por favor ingrese un valor válido')
     return
 } else{
 listaGeneral.push(nuevoNombre)
@@ -11,8 +14,31 @@ console.log(listaGeneral)
 mostrarLista();
 }
  }
+ // Subfunciones
+ // Uso de Html
  function mostrarLista(){
     let contenedor = document.getElementById("listaAmigos");
     contenedor.innerHTML = ""
-    listaGeneral
- }
+ listaGeneral.forEach(function(nombre){
+    let li = document.createElement("li")
+    li.textContent = nombre
+    contenedor.appendChild(li)
+ })
+
+
+};
+function sortearAmigo() {
+if(listaGeneral.length===0){
+    alert("No hay amigos para sortear")
+}else{
+    let indice = Math.floor(Math.random() * listaGeneral.length);
+    let elegido = listaGeneral.splice(indice, 1)[0];
+/*
+//Por si deseamos que reemplaze h2
+document.querySelector("h2").innerHTML = `El amigo secreto es: ${elegido}`
+*/
+// Cuando queremos a la alerta
+    alert(`El amigo secreto es: ${elegido}`);
+    mostrarLista();
+}
+}
--- readme.md
+++ readme.md
@@ -11,7 +11,23 @@ a- Alerta si el campo esta vacio, Return para detener accion
 b- Agregar el nombre a la lista si cumple con la condicion 
 c- Para luego limpiar la casilla input, Con value = ''
 
--Estado del proyecto: Fin hoy Lunes,11 de agosto
 
-- Estado del proyecto: Martes 12 de agosto
 
+- Estado del proyecto:  Terminado,Martes 12 de agosto
+
+Hoy se realizo:
+
+Se hizo uso de herramientas html como <li>
+para la funcion mostrarLista()
+
+a-Contenedor.innerHTML='' 
+b-ForEach para asignar a cada elemento
+c-Appendchild para copiar el elemento li en en el contenedor
+
+
+Función sorteo
+
+a- alerta para valor nulo
+b- else para sortear con math.floor(math.random()*ListaGeneral.legtch)
+c- Creacion de variables de indice y el elegido
+d- Mensaje final con la variable elegido
